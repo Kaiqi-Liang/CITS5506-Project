bf7f96c74aa86b23cb1b120d53015a9cf8f734f7
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-09-06 17:21:59 +0800

    Send recording

--- client.py
+++ client.py
@@ -1,19 +1,24 @@
 from gpiozero import Button
 import socket
 import time
+import os
+
 btn = Button(4)
-mySocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 HOST='192.168.137.46'
 PORT=8888
+
 try:
-	mySocket.connect((HOST,PORT))
-	print("connected")
+	client_socket.connect((HOST,PORT))
+	print('connected')
 except:
 	print('connect failed')
+
 while True:
 	btn.wait_for_press()
-	with open('.ssh/id_rsa.pub', 'r') as ssh:
-		key = ssh.read()
-		print(key)
-		mySocket.send(key.encode())
-	time.sleep(1)
+	os.system('arecord --format=S16_LE --rate=16000 --file-type=wav --duration=1 out.wav')
+	with open('out.wav', 'rb') as recording:
+		for chunk in recording:
+			client_socket.send(chunk)
+	print(client_socket.recv(1000))
