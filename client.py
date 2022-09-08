e9c461fef5dd90d5b030e953d576c00e6e1a82de
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-09-08 16:14:52 +0800

    Motor

--- client.py
+++ client.py
@@ -1,13 +1,22 @@
 from gpiozero import Button
+import RPi.GPIO as GPIO
 import socket
 import time
 import os
 
+GPIO.setmode(GPIO.BOARD)
 btn = Button(4)
+
+PIN_MOTOR = 11
+GPIO.setup(PIN_MOTOR, GPIO.OUT)
+servo = GPIO.PWM(PIN_MOTOR, 50)
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-HOST='192.168.137.46'
+HOST='172.20.10.2'
 PORT=8888
+locked = 2
+unlocked = 12
 
 try:
 	client_socket.connect((HOST,PORT))
@@ -15,10 +24,22 @@ try:
 except:
 	print('connect failed')
 
+servo.start(0)
+time.sleep(1)
 while True:
 	btn.wait_for_press()
-	os.system('arecord --format=S16_LE --rate=16000 --file-type=wav --duration=1 out.wav')
-	with open('out.wav', 'rb') as recording:
-		for chunk in recording:
-			client_socket.send(chunk)
-	print(client_socket.recv(1000))
+
+	servo.ChangeDutyCycle(locked)
+	time.sleep(1)
+
+	servo.ChangeDutyCycle(unlocked)
+	time.sleep(1)
+
+	servo.ChangeDutyCycle(locked)
+	time.sleep(1)
+
+	#os.system('arecord --format=S16_LE --rate=16000 --file-type=wav --duration=1 out.wav')
+	#with open('out.wav', 'rb') as recording:
+	#	for chunk in recording:
+	#		client_socket.send(chunk)
+	#print(client_socket.recv(1000))
