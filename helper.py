cecb0f51859088c7298500916839e4f5c9ac88b9
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-09-22 23:49:46 +0800

    Delete unused code

--- helper.py
+++ helper.py
@@ -19,22 +19,3 @@ def data_to_file(conn: socket.socket, filename: str, endswith: bytes, message: b
 				break
 			file.write(data)
 	conn.send(message)
-
-import smtplib
-
-# Import the email modules we'll need
-from email.message import EmailMessage
-
-msg = EmailMessage()
-msg.set_content('hi')
-
-# me == the sender's email address
-# you == the recipient's email address
-msg['Subject'] = 'hi'
-msg['From'] = 'kaiqi.liang@uwa.edu.au'
-msg['To'] = 'kaiqi.liang@unsw.edu.au'
-
-# Send the message via our own SMTP server.
-s = smtplib.SMTP('localhost', 7000)
-s.send_message(msg)
-s.quit()
