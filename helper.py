aa34982fb1a0fedcf0619665db89b4257f4f2b79
Kaiqi Liang <kaiqi.liang@outlook.com>
2022-09-24 18:32:47 +0800

    Change png to jpeg

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 import socket
 
 USER_ADDR_INFO = ('100.90.40.209', 9000)
-DOORBELL_ADDR_INFO = ('100.90.44.216', 8000)
+DOORBELL_ADDR_INFO = ('100.90.47.138', 8000)
 START_AUDIO = b'start sending audio'
 END_AUDIO = b'finish sending audio'
 END_IMAGE = b'finish sending image'
